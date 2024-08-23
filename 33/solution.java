18a8ff4f91de9c8d567012bb74ab36cb841ab861
mahmoudasem337 <109934276+mahmoudasem337@users.noreply.github.com>
2024-08-23 07:25:00 +0300

    problem solution

---
+++ 33/solution.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        //Binary Search
+        //Trick in know what's part we should search in
+        //O(log n)
+        int left = 0, right = nums.length - 1;
+
+        while (left <= right) {
+            int mid = (left + right) / 2;
+
+            if (nums[mid] == target) {
+                return mid;
+            }
+
+            if (nums[left] <= nums[mid]) {
+                if (nums[left] <= target && target < nums[mid]) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            } else {
+                if (nums[mid] < target && target <= nums[right]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+        }
+
+        return -1;
+    }
+}
