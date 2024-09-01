5d330f01dfd30e8526593af140bd8ef5d1dc6c16
mahmoudasem337 <109934276+mahmoudasem337@users.noreply.github.com>
2024-09-01 06:08:07 +0300

    problem solution

---
+++ 141/solution.java
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+//Detecting cycles : Have two pointers, where one pointer increments twice as much as the other, if the two pointers meet, means that there is a cycle
+        ListNode slow=head;
+        ListNode fast=head;
+        while(fast!=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+            if(slow.val==fast.val){
+                return true;
+
+            }
+        }
+        return false;
+    }
+}
