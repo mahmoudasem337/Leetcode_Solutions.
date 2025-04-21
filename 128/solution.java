9c9cbcdd019c5ba814598590c78b28ece7687d2b
mahmoudasem337 <109934276+mahmoudasem337@users.noreply.github.com>
2025-04-21 19:34:57 +0200

    added problem solution

---
+++ 128/solution.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int longestConsecutive(int[] nums) {
+        //O(N Log N),O(1)
+        if (nums.length == 0) return 0;
+        Arrays.sort(nums);
+        int longest = 1;
+        int current = 1;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] == nums[i - 1]) {
+                continue;
+            }
+            if (nums[i] == nums[i - 1] + 1) {
+                current += 1;
+            } else {
+                longest = Math.max(longest, current);
+                current = 1;
+            }
+        }
+        return Math.max(longest, current);
+    }
+}
